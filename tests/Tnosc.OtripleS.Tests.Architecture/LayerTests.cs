4f1051a3bf25d5aad06b29a9977c39039eaa20ef
ahedfi <ahmed.hedfi@gmail.com>
2025-07-28 07:57:52 +0100

    Architecture Tests -> PASS

---
+++ tests/Tnosc.OtripleS.Tests.Architecture/LayerTests.cs
@@ -0,0 +1,17 @@
+﻿/// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// -----------------------------------------------------------------------------------
+
+using System.Reflection;
+
+namespace Tnosc.OtripleS.Tests.Architecture;
+
+public partial class LayerTests
+{
+    private readonly Assembly _domainAssembly = typeof(Domain.AssemblyReference).Assembly;
+    private readonly Assembly _applicationAssembly = typeof(Application.AssemblyReference).Assembly;
+    private readonly Assembly _infrastructureAssembly = typeof(Infrastructure.AssemblyReference).Assembly;
+    private readonly Assembly _presentationAssembly = typeof(Api.AssemblyReference).Assembly;
+}

57662781c142914c57e7a5c4e3d23a271329f07a
ahedfi <ahmed.hedfi@gmail.com>
2025-07-28 20:36:33 +0100

    INFRA: Initialize Unit Tests Project #5

---
+++ tests/Tnosc.OtripleS.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,16 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using Xunit;
+
+namespace Tnosc.OtripleS.Tests.Unit;
+
+public class DeleteMe
+{
+    [Fact]
+    public void ShouldBeTrue() => Assert.True(true);
+    
+}

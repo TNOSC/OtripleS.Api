4f6412863d2f153d35c8cc72f3bcea940f88f395
ahedfi <ahmed.hedfi@gmail.com>
2025-07-31 21:52:01 +0100

    BROKERS: Create Web App

---
+++ devops/Tnosc.OtripleS.Server.Provision/Brokers/Clouds/CloudBroker.WebApp.cs
@@ -0,0 +1,53 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure;
+using Azure.ResourceManager;
+using Azure.ResourceManager.AppService;
+using Azure.ResourceManager.AppService.Models;
+using Azure.ResourceManager.Resources;
+
+namespace Tnosc.OtripleS.Server.Provision.Brokers.Clouds;
+
+public partial class CloudBroker
+{
+    public async ValueTask<WebSiteResource> CreateWebAppAsync(
+        string webAppName,
+        string databaseConnectionString,
+        AppServicePlanResource plan,
+        ResourceGroupResource resourceGroup)
+    {
+        WebSiteCollection webAppCollection = resourceGroup.GetWebSites();
+
+        var siteData = new WebSiteData(resourceGroup.Data.Location)
+        {
+            AppServicePlanId = plan.Id,
+            SiteConfig = new SiteConfigProperties()
+            {
+                NetFrameworkVersion = "v9.0" ,
+                ConnectionStrings = new List<ConnStringInfo>()
+                {
+                    new ConnStringInfo()
+                    {
+                        Name = "DefaultConnection",
+                        ConnectionString = databaseConnectionString,
+                        ConnectionStringType = ConnectionStringType.SqlAzure
+                    }
+                },
+            },
+        };
+
+        // Create or update the web app
+        ArmOperation<WebSiteResource> operation = await webAppCollection.CreateOrUpdateAsync(
+            WaitUntil.Completed,
+            webAppName,
+            siteData);
+
+        return operation.Value;
+    }
+}

7e3c8407ab43d96f64634cc8e4012cf8f885bb03
ahedfi <ahmed.hedfi@gmail.com>
2025-08-02 01:20:12 +0100

    PROCESSING: PROVISION

---
+++ devops/Tnosc.OtripleS.Server.Provision/Processings/ICloudManagementProcessingService.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace Tnosc.OtripleS.Server.Provision.Processings;
+
+internal interface ICloudManagementProcessingService
+{
+    ValueTask ProcessAsync();
+}

d80b2208852a631e39023692892879757a6be526
ahedfi <ahmed.hedfi@gmail.com>
2025-08-03 23:54:57 +0100

    Shared: Add Domain Library

---
+++ lib/Tnosc.Lib.Domain/IEntityId.cs
@@ -0,0 +1,11 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+namespace Tnosc.Lib.Domain;
+
+public interface IEntityId
+{
+}

7035a2b9f8f010d58e345a196aadcd201ed66920
ahedfi <ahmed.hedfi@gmail.com>
2025-08-03 23:56:29 +0100

    Domain: Add Student #22

---
+++ src/Tnosc.OtripleS.Server.Domain/Students/Student.cs
@@ -0,0 +1,25 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System;
+using Tnosc.Lib.Domain;
+
+namespace Tnosc.OtripleS.Server.Domain.Students;
+
+public sealed class Student : Entity<StudentId>, IAuditable
+{
+    public string UserId { get; set; } = null!;
+    public string IdentityNumber { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string MiddleName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public DateTimeOffset BirthDate { get; set; }
+    public Gender Gender { get; set; }
+    public DateTimeOffset CreatedDate { get; set; }
+    public DateTimeOffset UpdatedDate { get; set; }
+    public Guid CreatedBy { get; set; }
+    public Guid UpdatedBy { get; set; }
+}

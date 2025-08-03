eb00ed6f50f946044ba7adfd82df7d019ba688f1
ahedfi <ahmed.hedfi@gmail.com>
2025-08-03 07:52:50 +0100

    Brokers: Display Env Variables

--- devops/Tnosc.OtripleS.Server.Provision/Brokers/Clouds/CloudBroker.cs
+++ devops/Tnosc.OtripleS.Server.Provision/Brokers/Clouds/CloudBroker.cs
@@ -7,6 +7,7 @@
 using System;
 using Azure.Identity;
 using Azure.ResourceManager;
+using Tnosc.OtripleS.Server.Provision.Brokers.Loggings;
 
 namespace Tnosc.OtripleS.Server.Provision.Brokers.Clouds;
 
@@ -15,9 +16,12 @@ internal sealed partial class CloudBroker : ICloudBroker
     private readonly ArmClient _armClient;
     private readonly string _adminName;
     private readonly string _adminAccess;
+    private readonly ILoggingBroker _loggingBroker;
+
 
     public CloudBroker()
     {
+        
         if ( Environment.GetEnvironmentVariable("AZURE_CLIENT_ID") is null)
         {
             throw new InvalidOperationException("AZURE_CLIENT_ID does not exist in environment variables");
@@ -31,10 +35,18 @@ internal sealed partial class CloudBroker : ICloudBroker
             throw new InvalidOperationException("AZURE_CLIENT_SECRET does not exist in environment variables");
         }
 
+        _loggingBroker = new LoggingBroker();
+
         _adminName = Environment.GetEnvironmentVariable("AzureAdminName") 
             ?? throw new InvalidOperationException("AzureAdminName does not exist in environment variables");
         _adminAccess = Environment.GetEnvironmentVariable("AzureAdminAccess") 
             ?? throw new InvalidOperationException("AzureAdminAccess does not exist in environment variables");
+
+        _loggingBroker.LogActivity(message: $"AZURE_CLIENT_ID {Environment.GetEnvironmentVariable("AZURE_CLIENT_ID")}");
+        _loggingBroker.LogActivity(message: $"AZURE_TENANT_ID {Environment.GetEnvironmentVariable("AZURE_TENANT_ID")}");
+        _loggingBroker.LogActivity(message: $"AZURE_CLIENT_SECRET {Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET")}");
+        _loggingBroker.LogActivity(message: $"AzureAdminName {Environment.GetEnvironmentVariable("AzureAdminName")}");
+        _loggingBroker.LogActivity(message: $"AZURE_CLIENT_SECRET {Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET")}");
 
         _armClient = new ArmClient(new DefaultAzureCredential());
     }

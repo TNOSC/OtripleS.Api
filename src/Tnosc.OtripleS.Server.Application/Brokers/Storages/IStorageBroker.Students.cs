94c721ce71af2fa3cd09ce6f65c7b9fcdadd1924
ahedfi <ahmed.hedfi@gmail.com>
2025-08-05 20:38:46 +0100

    Brokers: Add Student Broker

---
+++ src/Tnosc.OtripleS.Server.Application/Brokers/Storages/IStorageBroker.Students.cs
@@ -0,0 +1,21 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tnosc.OtripleS.Server.Domain.Students;
+
+namespace Tnosc.OtripleS.Server.Application.Brokers.Storages;
+
+public partial interface IStorageBroker
+{
+    ValueTask<Student> InsertStudentAsync(Student student);
+    ValueTask<IEnumerable<Student>> SelectAllStudentsAsync();
+    ValueTask<Student> SelectStudentByIdAsync(Guid studentId);
+    ValueTask<Student> UpdateStudentAsync(Student student);
+    ValueTask<Student> DeleteStudentAsync(Student student);
+}
--- src/Tnosc.OtripleS.Server.Infrastructure/Brokers/Storages/StorageBroker.Students.cs
+++ src/Tnosc.OtripleS.Server.Infrastructure/Brokers/Storages/StorageBroker.Students.cs
@@ -4,6 +4,9 @@
 // Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
 // ----------------------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Tnosc.OtripleS.Server.Domain.Students;
 
@@ -12,4 +15,19 @@ namespace Tnosc.OtripleS.Server.Infrastructure.Brokers.Storages;
 internal partial class StorageBroker
 {
     public DbSet<Student> Students { get; set; }
+
+    public async ValueTask<Student> InsertStudentAsync(Student student) =>
+           await InsertAsync(@object: student);
+
+    public async ValueTask<IEnumerable<Student>> SelectAllStudentsAsync() => 
+        await SelectAllAsync<Student>();
+
+    public async ValueTask<Student> SelectStudentByIdAsync(Guid studentId) =>
+        await SelectAsync<Student>(objectIds: studentId);
+
+    public async ValueTask<Student> UpdateStudentAsync(Student student) =>
+        await UpdateAsync(@object: student);
+
+    public async ValueTask<Student> DeleteStudentAsync(Student student) =>
+        await DeleteAsync(@object: student);
 }

2dd61b7c46dbfed1ddb9d2c779259405158a1018
ahedfi <ahmed.hedfi@gmail.com>
2025-08-05 20:59:23 +0100

    FOUNDATIONS: Add Student Service

---
+++ src/Tnosc.OtripleS.Server.Application/Services/Foundations/Students/IStudentService.cs
@@ -0,0 +1,22 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tnosc.OtripleS.Server.Application.Brokers.Storages;
+using Tnosc.OtripleS.Server.Domain.Students;
+
+namespace Tnosc.OtripleS.Server.Application.Services.Foundations.Students;
+
+public interface IStudentService
+{
+    ValueTask<Student> RegisterStudentAsync(Student student);
+    ValueTask<IEnumerable<Student>> RetrieveAllStudentsAsync();
+    ValueTask<Student> RetrieveStudentByIdAsync(Guid studentId);
+    ValueTask<Student> ModifyStudentAsync(Student student);
+    ValueTask<Student> RemoveStudentByIdAsync(Guid studentId);
+}
---
+++ src/Tnosc.OtripleS.Server.Application/Services/Foundations/Students/StudentService.cs
@@ -0,0 +1,30 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tnosc.OtripleS.Server.Domain.Students;
+
+namespace Tnosc.OtripleS.Server.Application.Services.Foundations.Students;
+
+internal sealed class StudentService : IStudentService
+{
+    public ValueTask<Student> ModifyStudentAsync(Student student) => 
+        throw new NotImplementedException();
+    
+    public ValueTask<Student> RegisterStudentAsync(Student student) => 
+        throw new NotImplementedException();
+    
+    public ValueTask<Student> RemoveStudentByIdAsync(Guid studentId) => 
+        throw new NotImplementedException();
+    
+    public ValueTask<IEnumerable<Student>> RetrieveAllStudentsAsync() => 
+        throw new NotImplementedException();
+    
+    public ValueTask<Student> RetrieveStudentByIdAsync(Guid studentId) => 
+        throw new NotImplementedException();
+}

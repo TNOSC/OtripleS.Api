dc90e6757b35a66ade9ed23abe72a899829c5d03
ahedfi <ahmed.hedfi@gmail.com>
2025-08-05 22:20:03 +0100

    ShouldRegisterStudentAsync -> FAIL

--- src/Tnosc.OtripleS.Server.Application/Services/Foundations/Students/StudentService.cs
+++ src/Tnosc.OtripleS.Server.Application/Services/Foundations/Students/StudentService.cs
@@ -7,12 +7,28 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Tnosc.OtripleS.Server.Application.Brokers.DateTimes;
+using Tnosc.OtripleS.Server.Application.Brokers.Loggings;
+using Tnosc.OtripleS.Server.Application.Brokers.Storages;
 using Tnosc.OtripleS.Server.Domain.Students;
 
 namespace Tnosc.OtripleS.Server.Application.Services.Foundations.Students;
 
-internal sealed class StudentService : IStudentService
+public sealed class StudentService : IStudentService
 {
+    private readonly IStorageBroker _storageBroker;
+    private readonly IDateTimeBroker _dateTimeBroker;
+    private readonly ILoggingBroker _loggingBroker;
+
+    public StudentService(
+        IStorageBroker storageBroker,
+        IDateTimeBroker dateTimeBroker,
+        ILoggingBroker loggingBroker)
+    {
+        _storageBroker = storageBroker;
+        _dateTimeBroker = dateTimeBroker;
+        _loggingBroker = loggingBroker;
+    }
     public ValueTask<Student> ModifyStudentAsync(Student student) => 
         throw new NotImplementedException();
     
---
+++ tests/Tnosc.OtripleS.Server.Tests.Unit/Services/Foundations/Students/StudentServiceTests.Logic.Register.cs
@@ -0,0 +1,51 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using NSubstitute;
+using Shouldly;
+using Tnosc.OtripleS.Server.Domain.Students;
+using Xunit;
+
+namespace Tnosc.OtripleS.Server.Tests.Unit.Services.Foundations.Students;
+
+public partial class StudentServiceTests
+{
+    [Fact]
+    public async Task ShouldRegisterStudentAsync()
+    {
+        // given
+        DateTimeOffset randomDateTime = GetRandomDateTime();
+        DateTimeOffset dateTime = randomDateTime;
+        Student randomStudent = CreateRandomStudent(randomDateTime);
+        randomStudent.UpdatedBy = randomStudent.CreatedBy;
+        Student inputStudent = randomStudent;
+        Student storageStudent = randomStudent;
+        Student expectedStudent = storageStudent.DeepClone();
+
+        _dateTimeBrokerMock.GetCurrentDateTime().Returns(dateTime);
+
+        _storageBrokerMock.InsertStudentAsync(inputStudent)
+                     .Returns(storageStudent);
+
+        // when
+        Student actualStudent =
+            await _studentService.RegisterStudentAsync(inputStudent);
+
+        // then
+        actualStudent.ShouldBe(expectedStudent);
+
+        _dateTimeBrokerMock.Received(1).GetCurrentDateTime();
+        await _storageBrokerMock.Received(1).InsertStudentAsync(inputStudent);
+
+        _storageBrokerMock.ReceivedCalls().Count().ShouldBe(1);
+        _dateTimeBrokerMock.ReceivedCalls().Count().ShouldBe(1);
+        _loggingBrokerMock.ReceivedCalls().ShouldBeEmpty();
+    }
+}

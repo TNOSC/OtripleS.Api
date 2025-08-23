86e1c31a2ecac6e8cf69ef059a8ad6648f198cc6
ahedfi <ahmed.hedfi@gmail.com>
2025-08-23 17:11:49 +0100

    ShouldReturnBadRequestOnDeleteIfValidationErrorOccurredAsync -> FAIL

---
+++ tests/Tnosc.OtripleS.Server.Tests.Unit/Controllers/Students/Delete/StudentsControllerTests.Exceptions.cs
@@ -0,0 +1,50 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using RESTFulSense.Models;
+using Shouldly;
+using Tnosc.OtripleS.Server.Application.Exceptions.Foundations.Students;
+using Tnosc.OtripleS.Server.Domain.Students;
+using Xeptions;
+using Xunit;
+
+namespace Tnosc.OtripleS.Server.Tests.Unit.Controllers.Students;
+
+public partial class StudentsControllerTests
+{
+    [Theory]
+    [MemberData(nameof(ValidationExceptions))]
+    public async Task ShouldReturnBadRequestOnDeleteIfValidationErrorOccurredAsync(Xeption validationException)
+    {
+        // given
+        var someStudentId = Guid.NewGuid();
+
+        BadRequestObjectResult expectedBadRequestObjectResult =
+            BadRequest(exception: validationException.InnerException);
+
+        var expectedActionResult =
+            new ActionResult<Student>(expectedBadRequestObjectResult);
+
+        _studentService.RemoveStudentByIdAsync(studentId: someStudentId)
+            .ThrowsAsync(ex: validationException);
+
+        // when
+        ActionResult<Student> actualActionResult =
+            await _studentsController.DeleteStudentAsync(studentId: someStudentId);
+
+        // then
+        actualActionResult.ShouldBeEquivalentTo(
+            expected: expectedActionResult);
+
+        await _studentService.Received(1)
+            .RemoveStudentByIdAsync(studentId: someStudentId);
+    }
+}

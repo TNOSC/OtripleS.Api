d2636b439dc27696469b4c04e1c8a54bfc8ea9f4
ahedfi <ahmed.hedfi@gmail.com>
2025-08-23 19:18:15 +0100

    ShouldReturnBadRequestOnGetByIdIfValidationErrorOccurredAsync -> FAIL

---
+++ tests/Tnosc.OtripleS.Server.Tests.Unit/Controllers/Students/GetById/StudentsControllerTests.Exceptions.cs
@@ -0,0 +1,50 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using RESTFulSense.Models;
+using Shouldly;
+using Tnosc.OtripleS.Server.Application.Exceptions.Foundations.Students;
+using Tnosc.OtripleS.Server.Domain.Students;
+using Xeptions;
+using Xunit;
+
+namespace Tnosc.OtripleS.Server.Tests.Unit.Controllers.Students;
+
+public partial class StudentsControllerTests
+{
+    [Theory]
+    [MemberData(nameof(ValidationExceptions))]
+    public async Task ShouldReturnBadRequestOnGetByIdIfValidationErrorOccurredAsync(Xeption validationException)
+    {
+        // given
+        var someStudentId = Guid.NewGuid();
+
+        BadRequestObjectResult expectedBadRequestObjectResult =
+            BadRequest(exception: validationException.InnerException);
+
+        var expectedActionResult =
+            new ActionResult<Student>(result: expectedBadRequestObjectResult);
+
+        _studentService.RetrieveStudentByIdAsync(studentId: someStudentId)
+            .ThrowsAsync(ex: validationException);
+
+        // when
+        ActionResult<Student> actualActionResult =
+            await _studentsController.GetStudentByIdAsync(studentId: someStudentId);
+
+        // then
+        actualActionResult.ShouldBeEquivalentTo(
+            expected: expectedActionResult);
+
+        await _studentService.Received(requiredNumberOfCalls: 1)
+            .RetrieveStudentByIdAsync(studentId: someStudentId);
+    }
+}

76364cc285a641d3437edc545cc51a3df219fa52
ahmed <ahmed.hedfi@gmail.com>
2025-10-16 16:53:13 +0100

    ShouldAddLibraryCardAsync -> FAIL

---
+++ src/Tnosc.OtripleS.Server.Application/Services/Foundations/LibraryCards/ILibraryCardService.cs
@@ -0,0 +1,15 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Tnosc.OtripleS.Server.Domain.LibraryCards;
+
+namespace Tnosc.OtripleS.Server.Application.Services.Foundations.LibraryCards;
+
+public interface ILibraryCardService
+{
+    ValueTask<LibraryCard> AddLibraryCardAsync(LibraryCard libraryCard);
+}
---
+++ tests/Tnosc.OtripleS.Server.Tests.Unit/Services/Foundations/LibraryCards/LibraryCardServiceTests.cs
@@ -0,0 +1,43 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System;
+using NSubstitute;
+using Tnosc.OtripleS.Server.Application.Brokers.Storages;
+using Tnosc.OtripleS.Server.Application.Services.Foundations.LibraryCards;
+using Tnosc.OtripleS.Server.Domain.LibraryCards;
+using Tynamix.ObjectFiller;
+
+namespace Tnosc.OtripleS.Server.Tests.Unit.Services.Foundations.LibraryCards;
+
+public partial class LibraryCardServiceTests
+{
+    private readonly IStorageBroker _storageBrokerMock;
+    private readonly ILibraryCardService _libraryCardService;
+
+    public LibraryCardServiceTests()
+    {
+        _storageBrokerMock = Substitute.For<IStorageBroker>();
+
+        _libraryCardService = new LibraryCardService(
+             storageBroker: _storageBrokerMock);
+    }
+
+    private static LibraryCard CreateRandomLibraryAcocunt() =>
+        CreateLibraryCardFiller().Create();
+
+    private static Filler<LibraryCard> CreateLibraryCardFiller()
+    {
+        var filler = new Filler<LibraryCard>();
+
+        filler.Setup()
+            .OnType<DateTimeOffset>().Use(GetRandomDateTime());
+
+        return filler;
+    }
+    private static DateTimeOffset GetRandomDateTime() =>
+           new DateTimeRange(earliestDate: DateTime.UtcNow).GetValue();
+}
